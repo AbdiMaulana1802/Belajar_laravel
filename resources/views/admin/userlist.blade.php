f001da8b429b3856973fd5e8fcd62bcbb18560ad
Abdi maualana <63388851+AbdiMaulana1802@users.noreply.github.com>
2020-07-26 12:52:23 +0700

    Add files via upload

---
+++ resources/views/admin/userlist.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+<!-- JS, Popper.js, and jQuery -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+    <title>userlist</title>
+</head>
+<body>
+    <div class="container">
+    
+    <table class="table table-bordered">
+        <tr>
+            <td>No</td>
+            <td>Nama User</td>
+            <td>Email</td>
+            <td>Status</td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>({$user1})</td>
+            <td>abdimaulana1602@gmail.com</td>
+            <td>pelajar</td>
+        </tr>
+        <tr>
+            <td>2</td>
+            <td>({$user2})</td>
+            <td>muhhamadrivaldi123@gmail.com</td>
+            <td>pelajar</td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>({$user3})</td>
+            <td>fadlankasyfi345@gmail.com</td>
+            <td>pelajar</td>
+        </tr>
+    </table>
+    </div>
+</body>
+</html>
